69cb94f75b473819faf027c2b07488d7fb34343a
truptii-06 <141917519+truptii-06@users.noreply.github.com>
2025-02-23 18:41:09 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+export const itemData = [{
+        item_img: "/item/Biryani.jpg"
+
+    },
+    {
+        item_img: "/item/pasta.jpg"
+    },
+    {
+        item_img: "/item/Icecream.jpg"
+    },
+    {
+        item_img: "/item/pizza.jpg"
+    },
+    {
+        item_img: "/item/khichdi.jpg"
+    },
+    {
+        item_img: "/item/Noodles.jpg"
+    },
+    {
+        item_img: "/item/shake.jpg"
+    },
+    {
+        item_img: "/item/burger.jpg"
+    },
+    {
+        item_img: "/item/shwarma.jpg"
+    },
+
+]
